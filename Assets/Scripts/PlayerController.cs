13fb1ffd02f52cf50a93d03fa3f64ae444a7717a
Clijmart <clijmart@gmail.com>
2022-10-20 21:36:28 +0200

    Removed imported assets and created custom player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 playerMovementInput;
+    private Vector2 playerMouseInput;
+    private bool isGrounded;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private LayerMask solidMask;
+    [SerializeField] Transform feetTransform;
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private Rigidbody playerBody;
+    [Space]
+    [SerializeField] private float speed;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float cameraAngle;
+
+    private void Update()
+    {
+        playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (playerMovementInput.x != 0 || playerMovementInput.z != 0)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+        if (Physics.CheckSphere(feetTransform.position, 0.1f, solidMask))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+
+        playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), 0f);
+
+        MovePlayer();
+        MovePlayerCamera();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 moveVector = transform.TransformDirection(playerMovementInput) * speed;
+        playerBody.velocity = new Vector3(moveVector.x, playerBody.velocity.y, moveVector.z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                playerBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void MovePlayerCamera()
+    {
+        transform.Rotate(0f, playerMouseInput.x * sensitivity, 0f);
+        playerCamera.transform.localRotation = Quaternion.Euler(cameraAngle, 0f, 0f);
+    }
+}
